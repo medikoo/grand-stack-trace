9e9fc2c54b5c21154739913d4328f570cfe19af7
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 17:37:33 +0200

    test: whitespace cleanup

--- test/configure/promise.js
+++ test/configure/promise.js
@@ -10,7 +10,6 @@ test("Should", t => {
 	Promise.resolve()
 		.then(() => {
 			const stackItems = new Error("Test").stack.split("\n");
-
 			t.test("Bridge stacks among 'then'", t => {
 				t.equal(stackItems[1].endsWith(`${ __filename }:12:23)`), true);
 				t.equal(stackItems[2], "From previous event:");
@@ -23,18 +22,18 @@ test("Should", t => {
 		.catch(() => {
 			const stackItems = new Error("Test").stack.split("\n");
 			t.test("Bridge stacks among 'catch'", t => {
-				t.equal(stackItems[1].endsWith(`${ __filename }:24:23)`), true);
+				t.equal(stackItems[1].endsWith(`${ __filename }:23:23)`), true);
 				t.equal(stackItems[2], "From previous event:");
-				t.equal(stackItems[3].endsWith(`${ __filename }:23:9)`), true);
+				t.equal(stackItems[3].endsWith(`${ __filename }:22:9)`), true);
 				t.end();
 			});
 		})
 		.finally(() => {
 			const stackItems = new Error("Test").stack.split("\n");
 			t.test("Bridge stacks among 'finally'", t => {
-				t.equal(stackItems[1].endsWith(`${ __filename }:33:23)`), true);
+				t.equal(stackItems[1].endsWith(`${ __filename }:32:23)`), true);
 				t.equal(stackItems[2], "From previous event:");
-				t.equal(stackItems[3].endsWith(`${ __filename }:32:11)`), true);
+				t.equal(stackItems[3].endsWith(`${ __filename }:31:11)`), true);
 				t.end();
 			});
 			restore();
