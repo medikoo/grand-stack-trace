71f2ab51b207736e80a5c127f5de980e980562c3
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 12:59:45 +0200

    refactor: configure timer utils via configureTimer internal util

--- set-interval.js
+++ set-interval.js
@@ -1,6 +1,3 @@
 "use strict";
 
-const global    = require("es5-ext/global")
-    , configure = require("./configure/async-method");
-
-module.exports = configure(global, "setInterval", { callbackIndex: 0 });
+module.exports = require("./lib/configure-timer")("setInterval");
