647e0871f66626054bb3458b038abff84d31fd39
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 15:12:21 +0200

    refactor: configure nextTick directly via initAsyncHandler

--- next-tick.js
+++ next-tick.js
@@ -1,5 +1,26 @@
 "use strict";
 
-const configure = require("./configure/async-method");
+const ensureCallable = require("es5-ext/object/valid-callable")
+    , init           = require("./configure/init-async-handler");
 
-module.exports = configure(process, "nextTick", { callbackIndex: 0 });
+require("./configure/internal-file-names").add(__filename);
+
+const asyncMethod = process.nextTick;
+
+const asyncMethodWrapper = function (handler, ...args) {
+	ensureCallable(handler);
+	const { before, after } = init();
+	return asyncMethod.call(
+		this,
+		function (...callbackArgs) {
+			before();
+			try { return handler.call(this, ...callbackArgs); }
+			finally { after(); }
+		},
+		...args
+	);
+};
+module.exports = {
+	setup() { process.nextTick = asyncMethodWrapper; },
+	restore() { process.nextTick = asyncMethod; }
+};
