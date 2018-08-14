f0f34cadc7015437b8cc2491e71af227c17518c1
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 17:41:29 +0200

    feat: general node.js hook based on async_hooks

---
+++ node.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const asyncHooks = require("async_hooks")
+    , init       = require("./configure/init-async-handler");
+
+require("./configure/internal-file-names")
+	.add(__filename)
+	.add("internal/async_hooks.js")
+	.add("internal/timers.js")
+	.add("internal/process/next_tick.js")
+	.add("timers.js");
+
+const hooks = new Map();
+
+const after = id => {
+	const hook = hooks.get(id);
+	if (!hook) return;
+	hook.after();
+	hooks.delete(id);
+};
+const asyncHook = asyncHooks.createHook({
+	init(id) { hooks.set(id, init()); },
+	before(id) {
+		const hook = hooks.get(id);
+		if (hook) hook.before();
+	},
+	after,
+	destroy: after,
+	promiseResolve: after
+});
+
+module.exports = { setup: () => asyncHook.enable(), restore: () => asyncHook.disable() };
