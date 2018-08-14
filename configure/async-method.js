34d47437cb09d61daa8b96b704efe0da07ba13e6
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 12:58:26 +0200

    refactor: seclude initAsyncHandler generic util

--- configure/async-method.js
+++ configure/async-method.js
@@ -1,72 +1,12 @@
 "use strict";
 
-// For this to work all stack trace frames need to be exposed
-Error.stackTraceLimit = Infinity;
-
 const ensureObject        = require("es5-ext/object/valid-object")
     , ensureNaturalNumber = require("es5-ext/object/ensure-natural-number-value")
     , ensurePlainFunction = require("es5-ext/object/ensure-plain-function")
-    , ensurePropertyKey   = require("es5-ext/object/valid-value");
+    , ensurePropertyKey   = require("es5-ext/object/valid-value")
+    , init                = require("./init-async-handler");
 
-let bridge = null;
-
-const filterInternalTrace = structuredStackTrace =>
-	structuredStackTrace.filter(callSite => callSite.getFileName() !== __filename);
-
-const prepareDrop = (error, structuredStackTrace) => filterInternalTrace(structuredStackTrace);
-
-const isCallSiteSame = (callSiteA, callSiteB) =>
-	callSiteA.getFileName() === callSiteB.getFileName() &&
-	callSiteA.getLineNumber() === callSiteB.getLineNumber() &&
-	callSiteA.getColumnNumber() === callSiteB.getColumnNumber();
-
-const findDropLength = current => {
-	const { drop } = bridge, dropLastIndex = drop.length - 1, currentLastIndex = current.length - 1;
-	let length = 0;
-	while (
-		drop[dropLastIndex - length] &&
-		isCallSiteSame(drop[dropLastIndex - length], current[currentLastIndex - length])
-	) {
-		++length;
-	}
-
-	return length;
-};
-
-const prepareStackTrace = (error, structuredStackTrace) => {
-	structuredStackTrace = filterInternalTrace(structuredStackTrace);
-
-	if (bridge) {
-		const dropLength = findDropLength(structuredStackTrace);
-		if (!dropLength) {
-			const message =
-				"Unexpected stack state: registered drop not present in current stack\n" +
-				`Drop:${ bridge.drop.join("\n") }\nStack:${ structuredStackTrace.join("\n") }`;
-			delete Error.prepareStackTrace;
-			throw new Error(message);
-		}
-
-		structuredStackTrace = structuredStackTrace.slice(0, -dropLength);
-	}
-
-	let stack = `${ error.name }${ error.message ? ": " : "" }${ error.message }\n${
-		structuredStackTrace.map(callSite => `    at ${ callSite }`).join("\n")
-	}`;
-	if (bridge) stack += `\nFrom previous event:\n${ bridge.stack }`;
-	return stack;
-};
-
-const getPreparedStack = prepare => {
-	const previousPrepare = Error.prepareStackTrace;
-	Error.prepareStackTrace = prepare;
-	try { return new Error().stack; }
-	finally { Error.prepareStackTrace = previousPrepare; }
-};
-
-const getBareStack = () => {
-	const stack = getPreparedStack(prepareStackTrace);
-	return stack.slice(stack.indexOf("\n") + 1);
-};
+require("./internal-file-names").add(__filename);
 
 module.exports = (object, asyncMethodName, { callbackIndex }) => {
 	ensureObject(object);
@@ -77,18 +17,11 @@ module.exports = (object, asyncMethodName, { callbackIndex }) => {
 
 	const asyncMethodWrapper = function (...asyncMethodArgs) {
 		const callback = ensurePlainFunction(asyncMethodArgs[callbackIndex]);
-		const previousStack = getBareStack();
+		const { before, after } = init();
 		asyncMethodArgs[callbackIndex] = function (...callbackArgs) {
-			const previousBridge = bridge;
-			bridge = { stack: previousStack, drop: getPreparedStack(prepareDrop) };
-			const previousPrepare = Error.prepareStackTrace;
-			Error.prepareStackTrace = prepareStackTrace;
-			try {
-				return callback.call(this, ...callbackArgs);
-			} finally {
-				bridge = previousBridge;
-				Error.prepareStackTrace = previousPrepare;
-			}
+			before();
+			try { return callback.call(this, ...callbackArgs); }
+			finally { after(); }
 		};
 		return asyncMethod.apply(this, asyncMethodArgs);
 	};
---
+++ configure/init-async-handler.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// For this to work all stack trace frames need to be exposed
+Error.stackTraceLimit = Infinity;
+
+const internalFileNames = require("./internal-file-names");
+internalFileNames.add(__filename);
+
+let bridge = null;
+
+const filterInternalTrace = structuredStackTrace =>
+	structuredStackTrace.filter(callSite => !internalFileNames.has(callSite.getFileName()));
+
+const prepareDrop = (error, structuredStackTrace) => filterInternalTrace(structuredStackTrace);
+
+const isCallSiteSame = (callSiteA, callSiteB) =>
+	callSiteA.getFileName() === callSiteB.getFileName() &&
+	callSiteA.getLineNumber() === callSiteB.getLineNumber() &&
+	callSiteA.getColumnNumber() === callSiteB.getColumnNumber();
+
+const findDropLength = current => {
+	const { drop } = bridge, dropLastIndex = drop.length - 1, currentLastIndex = current.length - 1;
+	let length = 0;
+	while (
+		drop[dropLastIndex - length] &&
+		isCallSiteSame(drop[dropLastIndex - length], current[currentLastIndex - length])
+	) {
+		++length;
+	}
+
+	return length;
+};
+
+const prepareStackTrace = (error, structuredStackTrace) => {
+	structuredStackTrace = filterInternalTrace(structuredStackTrace);
+
+	if (bridge) {
+		const dropLength = findDropLength(structuredStackTrace);
+		if (!dropLength) {
+			const message =
+				"Unexpected stack state: registered drop not present in current stack\n" +
+				`Drop:${ bridge.drop.join("\n") }\nStack:${ structuredStackTrace.join("\n") }`;
+			delete Error.prepareStackTrace;
+			throw new Error(message);
+		}
+
+		structuredStackTrace = structuredStackTrace.slice(0, -dropLength);
+	}
+
+	let stack = `${ error.name }${ error.message ? ": " : "" }${ error.message }\n${
+		structuredStackTrace.map(callSite => `    at ${ callSite }`).join("\n")
+	}`;
+	if (bridge) stack += `\nFrom previous event:\n${ bridge.stack }`;
+	return stack;
+};
+
+const getPreparedStack = prepare => {
+	const previousPrepare = Error.prepareStackTrace;
+	Error.prepareStackTrace = prepare;
+	try { return new Error().stack; }
+	finally { Error.prepareStackTrace = previousPrepare; }
+};
+
+const getBareStack = () => {
+	const stack = getPreparedStack(prepareStackTrace);
+	return stack.slice(stack.indexOf("\n") + 1);
+};
+
+module.exports = () => {
+	const previousStack = getBareStack();
+	let previousBridge, previousPrepare;
+	return {
+		before() {
+			previousBridge = bridge;
+			bridge = { stack: previousStack, drop: getPreparedStack(prepareDrop) };
+			previousPrepare = Error.prepareStackTrace;
+			Error.prepareStackTrace = prepareStackTrace;
+		},
+		after() {
+			bridge = previousBridge;
+			Error.prepareStackTrace = previousPrepare;
+		}
+	};
+};
