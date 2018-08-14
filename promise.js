25a99265c85b105aa01c9e98ec833eec15092791
Mariusz Nowak <medyk@medikoo.com>
2018-08-14 16:33:43 +0200

    feat: native Promise hook

---
+++ promise.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = require("./configure/promise")(Promise);
---
+++ test/promise.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const test               = require("tape")
+    , { setup, restore } = require("../promise");
+
+test("Should", t => {
+	setup();
+
+	Promise.resolve().then(() => {
+		const stackItems = new Error("Test").stack.split("\n");
+
+		t.test("Bridge stacks among 'then'", t => {
+			t.equal(stackItems[1].endsWith(`${ __filename }:10:22)`), true);
+			t.equal(stackItems[2], "From previous event:");
+			t.equal(stackItems[3].endsWith(`${ __filename }:9:20)`), true);
+			t.end();
+		});
+
+		restore();
+		t.end();
+	});
+});
