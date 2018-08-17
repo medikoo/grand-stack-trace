9fe3c99122d6f7c579ca123971b02e9555300e88
Mariusz Nowak <medyk@medikoo.com>
2018-08-17 16:06:37 +0200

    feat: ensure Promise hook covers all methods

--- test/promise.js
+++ test/promise.js
@@ -1,22 +1,29 @@
 "use strict";
 
-const test               = require("tape")
+const test                = require("tape")
     , { setup, teardown } = require("../promise");
 
 test("Should", t => {
 	setup();
 
-	Promise.resolve().then(() => {
-		const stackItems = new Error("Test").stack.split("\n");
-
-		t.test("Bridge stacks among 'then'", t => {
-			t.equal(stackItems[1].endsWith(`${ __filename }:10:22)`), true);
-			t.equal(stackItems[2], "From previous event:");
-			t.equal(stackItems[3].endsWith(`${ __filename }:9:20)`), true);
+	Promise.all([{ then(onFulfilled) { onFulfilled(new Error("Test").stack.split("\n")); } }])
+		.then(([stackItems]) => {
+			t.test("Bridge stacks among 'Promise.all'", t => {
+				t.equal(stackItems[1].endsWith(`${ __filename }:9:49)`), true);
+				t.equal(stackItems[2], "From previous event:");
+				t.equal(stackItems[3].endsWith(`${ __filename }:9:10)`), true);
+				t.end();
+			});
+			return new Error("Test").stack.split("\n");
+		})
+		.then(stackItems => {
+			t.test("Bridge stacks among 'promise.then'", t => {
+				t.equal(stackItems[1].endsWith(`${ __filename }:17:11)`), true);
+				t.equal(stackItems[2], "From previous event:");
+				t.equal(stackItems[3].endsWith(`${ __filename }:10:4)`), true);
+				t.end();
+			});
+			teardown();
 			t.end();
 		});
-
-		teardown();
-		t.end();
-	});
 });
