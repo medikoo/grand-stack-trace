092b6be7876a67f82f823b98af498c0207bc0eb2
Mariusz Nowak <medyk@medikoo.com>
2019-02-20 09:38:42 +0100

    docs: improve comment

--- configure/init-async-hook.js
+++ configure/init-async-hook.js
@@ -43,8 +43,8 @@ const findDropLength = current => {
 
 const prepareStackTrace = (error, structuredStackTrace) => {
 	// Disconnected is when stack for error had been retrieved in different async context
-	// from one where error was initialized
-	// (until V8 introduces dedicated hooks for error initialization we can't workaround it)
+	// than one in which error was initialized
+	// (until V8 introduces dedicated hook for error intialization there seems to be no workaround)
 	let isDisconnected = false;
 	if (bridge && bridge.drop) {
 		const dropLength = findDropLength(structuredStackTrace);
